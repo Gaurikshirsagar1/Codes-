348df02e848f3ebbf2f1b1ca8feee34826d9a198
Gauri Kshirsagar <gauriverse@gmail.com>
2026-05-10 18:48:39 +0530

    Modify input values and fix output formatting
    Updated data arrays and corrected output statements.

--- HPC_4.cpp
+++ HPC_4.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n = 5;
+int main(){
+int n=5;
 
-    double x[5] = {1, 2, 3, 4, 5};
-    double y[5] = {2, 4, 5, 4, 5};
+double x[5]={1,2,3,5,6};
+double y[5]={8,4,3,5,1};
 
-    double sum_x = 0, sum_y = 0, sum_xy = 0, sum_x2 = 0;
+double sum_x=0;
+double sum_y=0;
+double sum_xy=0;
+double sum_x2=0;
 
-    // Sequential computation
-    for (int i = 0; i < n; i++) {
-        sum_x += x[i];             // sum of x values
-        sum_y += y[i];             // sum of y values
-        sum_xy += x[i] * y[i];     // sum of x*y
-        sum_x2 += x[i] * x[i];     // sum of x^2
-    }
+for(int i=0;i<n;i++){
+    sum_x+=x[i];
+    sum_y+=y[i];
+    sum_xy+=x[i]*y[i];
+    sum_x2+=x[i]*x[i];
+}
 
-    // Calculate slope (m) and intercept (b)
-    double m = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - sum_x * sum_x);
-    double b = (sum_y - m * sum_x) / n;
+double m=(n*sum_xy - sum_x *sum_y)/ (n*sum_x2 - sum_x*sum_x);
+double b=(sum_y -m *sum_x)/n;
 
-    cout << "Slope (m): " << m << endl;
-    cout << "Intercept (b): " << b << endl;
+cout<<"value of slope"<<m;
+cout<< "value of intercept- "<<n;
 
-    return 0;
+
+
 }
